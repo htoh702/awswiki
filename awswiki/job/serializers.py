735dd1904236982b5c148d966f5a66ce5e6fb9ee
htoh702 <htoh702@gmail.com>
2024-04-23 08:55:54 +0900

    add search api

--- awswiki/job/serializers.py
+++ awswiki/job/serializers.py
@@ -10,3 +10,12 @@ class job_reviewsSerializer(serializers.ModelSerializer):
     class Meta :
         model = job_reviews
         fields = '__all__'
+
+class searchSerializer(serializers.Serializer):
+    index = serializers.IntegerField()
+    tag = serializers.CharField()
+    title = serializers.CharField(required=False)
+    writer = serializers.CharField(required=False)
+    content = serializers.CharField(required=False)
+    date = serializers.DateTimeField()
+    image = serializers.FileField()
--- awswiki/job/views.py
+++ awswiki/job/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
-from job.serializers import jobSerializer, job_reviewsSerializer
+from job.serializers import jobSerializer, job_reviewsSerializer, searchSerializer
 from job.models import job, job_reviews
+
+from note.models import note
+from photo.models import photo
 
 class JobAPI(viewsets.ModelViewSet):
     queryset = job.objects.all()
     serializer_class = jobSerializer
+
+    @action(detail=False, methods=['get'])
+    def AllSearch(self, request):
+        search = request.query_params.get('search')
+        if not search:
+            return Response("Search parameter 'search' is required.", status=status.HTTP_400_BAD_REQUEST)
+
+        job_results = job.objects.filter(tag__icontains=search) | job.objects.filter(title__icontains=search) | job.objects.filter(content__icontains=search)
+        note_results = note.objects.filter(tag__icontains=search) | note.objects.filter(title__icontains=search) | note.objects.filter(content__icontains=search)
+        photo_results = photo.objects.filter(tag__icontains=search)
+
+        search_results = list(job_results) + list(note_results) + list(photo_results)
+
+        serializer = searchSerializer(search_results, many=True)
+
+        return Response(serializer.data)
 
 class Job_reviewsAPI(viewsets.ModelViewSet):
     queryset = job_reviews.objects.all()
