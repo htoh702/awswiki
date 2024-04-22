9dc3ed6e6b6c465fabe0663be2f74d5e02fda100
hee-bin <gmlqls4732@naver.com>
2024-04-23 08:54:08 +0900

    [update] photo api get

--- awswiki/photo/urls.py
+++ awswiki/photo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PhotoAPI, PhotoDateAPI 
 
 urlpatterns = [
-    path('api/', PhotoAPI.as_view({'get': 'list'}), name='photo-list'), 
+    path('api/', PhotoAPI.as_view({'get': 'list', 'post': 'create'}), name='photo-list'),
     path('api/<date:date>/', PhotoDateAPI.as_view(), name='photo-by-date'),  
 ]
